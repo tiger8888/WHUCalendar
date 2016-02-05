ac386b8037460fd18e6147abb2d59a4f9cc8cbb0
tiger8888 <seekarmor@139.com>
2016-02-05 21:55:55 +0800

    修复农历显示问题

--- WHUCalendarView/Classes/WHUCalendarCal.m
+++ WHUCalendarView/Classes/WHUCalendarCal.m
@@ -14,6 +14,9 @@
 @interface WHUCalendarCal()
 {
     dispatch_queue_t _workQueue;
+    NSCalendar* _lunarCalendar;
+    NSArray* _lunarDays;
+    NSArray* _lunarMonths;
 }
 @property(nonatomic,strong) NSCalendar* curCalendar;
 @property(nonatomic,strong) NSString* curDateStr;
@@ -29,6 +32,15 @@
     self=[super init];
     if(self){
         _workQueue=dispatch_queue_create("WHUCalendarCal_Work_Queue", DISPATCH_QUEUE_SERIAL);
+        _lunarCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierChinese];
+        _lunarCalendar.locale = [NSLocale localeWithLocaleIdentifier:@"zh-CN"];
+        //农历日期名
+        _lunarDays =[NSArray arrayWithObjects:@"初一",@"初二",@"初三",@"初四",@"初五",@"初六",@"初七",@"初八",@"初九",@"初十",
+                              @"十一",@"十二",@"十三",@"十四",@"十五",@"十六",@"十七",@"十八",@"十九",@"二十",
+                              @"廿一",@"廿二",@"廿三",@"廿四",@"廿五",@"廿六",@"廿七",@"廿八",@"廿九",@"三十",nil];
+        
+        //农历月份名
+        _lunarMonths=  [NSArray arrayWithObjects:@"正",@"二",@"三",@"四",@"五",@"六",@"七",@"八",@"九",@"十",@"十一",@"腊",nil];
     }
     return self;
 }
@@ -288,7 +300,7 @@
 -(void)LunarForSolarYear:(WHUCalendarItem *)calendarDay andComponent:(NSDateComponents*)com{
     
     
-    NSString *solarYear = [self LunarForSolarYear:com.year Month:com.month Day:com.day];
+    NSString *solarYear = [self LunarForSolarYear:[_curCalendar dateFromComponents:com]];
     
     NSArray *solarYear_arr= [solarYear componentsSeparatedByString:@"-"];
     
@@ -419,99 +431,12 @@
     return nil;
 }
 
--(NSString *)LunarForSolarYear:(int)wCurYear Month:(int)wCurMonth Day:(int)wCurDay{
-    
-    //农历日期名
-    NSArray *cDayName =  [NSArray arrayWithObjects:@"*",@"初一",@"初二",@"初三",@"初四",@"初五",@"初六",@"初七",@"初八",@"初九",@"初十",
-                          @"十一",@"十二",@"十三",@"十四",@"十五",@"十六",@"十七",@"十八",@"十九",@"二十",
-                          @"廿一",@"廿二",@"廿三",@"廿四",@"廿五",@"廿六",@"廿七",@"廿八",@"廿九",@"三十",nil];
-    
-    //农历月份名
-    NSArray *cMonName =  [NSArray arrayWithObjects:@"*",@"正",@"二",@"三",@"四",@"五",@"六",@"七",@"八",@"九",@"十",@"十一",@"腊",nil];
-    
-    //公历每月前面的天数
-    const int wMonthAdd[12] = {0,31,59,90,120,151,181,212,243,273,304,334};
-    
-    //农历数据
-    const int wNongliData[100] = {2635,333387,1701,1748,267701,694,2391,133423,1175,396438
-        ,3402,3749,331177,1453,694,201326,2350,465197,3221,3402
-        ,400202,2901,1386,267611,605,2349,137515,2709,464533,1738
-        ,2901,330421,1242,2651,199255,1323,529706,3733,1706,398762
-        ,2741,1206,267438,2647,1318,204070,3477,461653,1386,2413
-        ,330077,1197,2637,268877,3365,531109,2900,2922,398042,2395
-        ,1179,267415,2635,661067,1701,1748,398772,2742,2391,330031
-        ,1175,1611,200010,3749,527717,1452,2742,332397,2350,3222
-        ,268949,3402,3493,133973,1386,464219,605,2349,334123,2709
-        ,2890,267946,2773,592565,1210,2651,395863,1323,2707,265877};
-    
-    int nTheDate,nIsEnd,m,k,n,i,nBit;
-    
-    
-    //计算到初始时间1921年2月8日的天数：1921-2-8(正月初一)
-    nTheDate = (wCurYear - 1921) * 365 + (wCurYear - 1921) / 4 + wCurDay + wMonthAdd[wCurMonth - 1] - 38;
-    
-    if((!(wCurYear % 4)) && (wCurMonth > 2))
-        nTheDate = nTheDate + 1;
-    
-    //计算农历天干、地支、月、日
-    nIsEnd = 0;
-    m = 0;
-    n=0;
-    k=0;
-    while(nIsEnd != 1)
-    {
-        if(wNongliData[m] < 4095)
-            k = 11;
-        else
-            k = 12;
-        n = k;
-        while(n>=0)
-        {
-            //获取wNongliData(m)的第n个二进制位的值
-            nBit = wNongliData[m];
-            for(i=1;i<n+1;i++)
-                nBit = nBit/2;
-            
-            nBit = nBit % 2;
-            
-            if (nTheDate <= (29 + nBit))
-            {
-                nIsEnd = 1;
-                break;
-            }
-            
-            nTheDate = nTheDate - 29 - nBit;
-            n = n - 1;
-        }
-        if(nIsEnd)
-            break;
-        m = m + 1;
-    }
-    wCurYear = 1921 + m;
-    wCurMonth = k - n + 1;
-    wCurDay = nTheDate;
-    if (k == 12)
-    {
-        if (wCurMonth == wNongliData[m] / 65536 + 1)
-            wCurMonth = 1 - wCurMonth;
-        else if (wCurMonth > wNongliData[m] / 65536 + 1)
-            wCurMonth = wCurMonth - 1;
-    }
-    
-    
-    //生成农历月
-    NSString *szNongliMonth;
-    if (wCurMonth < 1){
-        szNongliMonth = [NSString stringWithFormat:@"闰%@",(NSString *)[cMonName objectAtIndex:-1 * wCurMonth]];
-    }else{
-        szNongliMonth = (NSString *)[cMonName objectAtIndex:wCurMonth];
-    }
-    
-    //生成农历日
-    NSString *szNongliDay = [cDayName objectAtIndex:wCurDay];
-    
-    //合并
-    NSString *lunarDate = [NSString stringWithFormat:@"%@-%@",szNongliMonth,szNongliDay];
+-(NSString *)LunarForSolarYear:(NSDate*)date{
+    NSInteger day = [_lunarCalendar components:NSCalendarUnitDay fromDate:date].day;
+    NSString* dayStr=_lunarDays[day-1];
+    NSInteger month = [_lunarCalendar components:NSCalendarUnitMonth fromDate:date].month;
+    NSString* monthStr=_lunarMonths[month-1];
+    NSString *lunarDate = [NSString stringWithFormat:@"%@-%@",monthStr,dayStr];
     
     return lunarDate;
 }
